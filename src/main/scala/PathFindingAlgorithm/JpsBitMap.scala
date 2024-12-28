6fa9d85ea0d21c9fd5a39fc8de4bdc472895f0c2
gql <2412266175@qq.com>
2024-12-28 18:38:31 +0800

    achieve v0.1

--- src/main/scala/PathFindingAlgorithm/JpsBitMap.scala
+++ src/main/scala/PathFindingAlgorithm/JpsBitMap.scala
@@ -6,18 +6,18 @@ class JpsBitMap(grid: Array[Array[Byte]]) {
   final val BITSPERCELL = 32
   final val BITOFFSET = powerOfTwo(BITSPERCELL)
   final val BITMASK = ~(0xffffffff << BITOFFSET)
-  final val BLOCK = 0
+  final val OBSTACLE = 0
   final val EMPTY = 1
-  final val BLOCKCELL = 0x00000000
+  final val OBSTACLECELL = 0x00000000
   final val EMPTYCELL = 0xffffffff
   private val totRows = grid.length
   private val totCols = grid(0).length
   private val rows = (totRows >> BITOFFSET) + 1
   private val cols = (totCols >> BITOFFSET) + 1
-  private val mapSurroundedWithBlock: Array[Array[Int]] = Array.fill(totRows + 2, cols + 2)(BLOCKCELL)
-  private val mapSurroundedWithBlockReverseBit: Array[Array[Int]] = Array.fill(totRows + 2, cols + 2)(BLOCKCELL)
-  private val transposeMapSurroundedWithBlock: Array[Array[Int]] = Array.fill(totCols + 2, rows + 2)(BLOCKCELL)
-  private val transposeMapSurroundedWithBlockReverseBit: Array[Array[Int]] = Array.fill(totCols + 2, rows + 2)(BLOCKCELL)
+  private val mapSurroundedWithObstacle: Array[Array[Int]] = Array.fill(totRows + 2, cols + 2)(OBSTACLECELL)
+  private val mapSurroundedWithObstacleReverseBit: Array[Array[Int]] = Array.fill(totRows + 2, cols + 2)(OBSTACLECELL)
+  private val transposeMapSurroundedWithObstacle: Array[Array[Int]] = Array.fill(totCols + 2, rows + 2)(OBSTACLECELL)
+  private val transposeMapSurroundedWithObstacleReverseBit: Array[Array[Int]] = Array.fill(totCols + 2, rows + 2)(OBSTACLECELL)
 
   private def powerOfTwo(n: Int): Int = {
     require(n > 0 && (n & (n - 1)) == 0, "n must be a power of 2")
@@ -39,11 +39,11 @@ class JpsBitMap(grid: Array[Array[Byte]]) {
           if (index < totCols) {
             value = value | (grid(i)(index) << k)
           } else {
-            value = value | (BLOCK << k)
+            value = value | (OBSTACLE << k)
           }
         }
-        mapSurroundedWithBlock(i + 1)(j + 1) = value
-        mapSurroundedWithBlockReverseBit(i + 1)(j + 1) = Integer.reverse(value)
+        mapSurroundedWithObstacle(i + 1)(j + 1) = value
+        mapSurroundedWithObstacleReverseBit(i + 1)(j + 1) = Integer.reverse(value)
       }
     }
 
@@ -56,11 +56,11 @@ class JpsBitMap(grid: Array[Array[Byte]]) {
           if (index < totRows) {
             value = value | (grid(index)(i) << k)
           } else {
-            value = value | (BLOCK << k)
+            value = value | (OBSTACLE << k)
           }
         }
-        transposeMapSurroundedWithBlock(i + 1)(j + 1) = value
-        transposeMapSurroundedWithBlockReverseBit(i + 1)(j + 1) = Integer.reverse(value)
+        transposeMapSurroundedWithObstacle(i + 1)(j + 1) = value
+        transposeMapSurroundedWithObstacleReverseBit(i + 1)(j + 1) = Integer.reverse(value)
       }
     }
   }
@@ -74,10 +74,10 @@ class JpsBitMap(grid: Array[Array[Byte]]) {
     }
   }
 
-  private def surroundedWithBlock(map: Array[Array[Int]]):Array[Array[Int]]  = {
+  private def surroundedWithObstacle(map: Array[Array[Int]]):Array[Array[Int]]  = {
     val rows = map.length
     val cols = map(0).length
-    val newMap = Array.fill(rows + 2, cols + 2)(BLOCKCELL)
+    val newMap = Array.fill(rows + 2, cols + 2)(OBSTACLECELL)
     for (i <- 0 until rows) {
       for (j <- 0 until cols) {
         newMap(i + 1)(j + 1) = map(i)(j)
@@ -91,19 +91,19 @@ class JpsBitMap(grid: Array[Array[Byte]]) {
   private def getColumnPos(col: Int): Int = col >> BITOFFSET
 
   private def getPosInMap(x: Int, y: Int) = {
-    mapSurroundedWithBlock(x + 1)(getColumnPos(y) + 1)
+    mapSurroundedWithObstacle(x + 1)(getColumnPos(y) + 1)
   }
 
   private def getPosInMapReverseBit(x: Int, y: Int) = {
-    mapSurroundedWithBlockReverseBit(x + 1)(getColumnPos(y) + 1)
+    mapSurroundedWithObstacleReverseBit(x + 1)(getColumnPos(y) + 1)
   }
 
   private def getPosInTransposeMap(x: Int, y: Int) = {
-    transposeMapSurroundedWithBlock(y + 1)(getRowPos(x) + 1)
+    transposeMapSurroundedWithObstacle(y + 1)(getRowPos(x) + 1)
   }
 
   private def getPosInTransposeMapReverseBit(x: Int, y: Int) = {
-    transposeMapSurroundedWithBlockReverseBit(y + 1)(getRowPos(x) + 1)
+    transposeMapSurroundedWithObstacleReverseBit(y + 1)(getRowPos(x) + 1)
   }
 
   //低位取1，高位不变
